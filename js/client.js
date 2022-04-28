c96f0ead37a5b76966d3256b9a1e4208501cdc9c
Amarra Hong <amarrahong@gmail.com>
2022-04-28 10:14:06 -0700

    Adding JS code to begin the login/logoutfor client and server side

---
+++ js/client.js
@@ -0,0 +1,85 @@
+ready(function () {
+
+    console.log("Client script loaded.");
+
+    function ajaxGET(url, callback) {
+
+        const xhr = new XMLHttpRequest();
+        xhr.onload = function () {
+            if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+                //console.log('responseText:' + xhr.responseText);
+                callback(this.responseText);
+
+            } else {
+                console.log(this.status);
+            }
+        }
+        xhr.open("GET", url);
+        xhr.send();
+    }
+
+
+    const sub = document.getElementById("submit");
+    if (sub){
+    sub.addEventListener('click', (event) => {
+
+        console.log("button clicked");
+        event.preventDefault();
+        console.log("before ajax");
+        ajaxPOST("/login", (data) => {
+            if (data) {
+                
+                const jsondata = JSON.parse(data)
+                console.log(jsondata.status)
+                if (jsondata.status === "success") {
+                    window.location.replace("/profile");
+                } else {
+                    document.getElementById("errorMsg").innerHTML = dataParsed.msg;
+                }
+            }
+        }, 
+
+        {
+            email:document.getElementById("email").value,
+            password: document.getElementById("password").value,
+        }
+        );
+
+    });
+
+function ajaxPOST(url, callback, data) {
+
+    let params = typeof data == 'string' ? data : Object.keys(data).map(
+        function (k) { return encodeURIComponent(k) + '=' + encodeURIComponent(data[k]) }
+    ).join('&');
+    console.log("params in ajaxPOST", params);
+
+    const xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+        if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+            console.log('responseText:' + xhr.responseText);
+            callback(this.responseText);
+
+        } else {
+            console.log(this.status);
+        }
+    }
+    xhr.open("POST", url);
+    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.send(params);
+    console.log("accessed");
+}
+
+}});
+
+
+function ready(callback) {
+    if (document.readyState != "loading") {
+        callback();
+        console.log("ready state is 'complete'");
+    } else {
+        document.addEventListener("DOMContentLoaded", callback);
+        console.log("Listener was invoked");
+    }
+}
---
+++ server.js
@@ -0,0 +1,228 @@
+const express = require("express");
+const session = require("express-session");
+const app = express();
+const fs = require("fs");
+const {
+    JSDOM
+} = require("jsdom");
+const mysql = require('mysql');
+
+
+
+app.use("/js", express.static("./public/js"));
+app.use("/css", express.static("./public/css"));
+app.use("/imgs", express.static("./public/imgs"));
+app.use("/fonts", express.static("./public/fonts"));
+app.use("/html", express.static("./public/html"));
+app.use("/media", express.static("./public/media"));
+
+app.use(
+    session({
+        secret: "secrets",
+        name: "candysession",
+        resave: false,
+        // create a unique identifier
+        saveUninitialized: true,
+    })
+);
+
+
+app.get("/", function (req, res) {
+    if (req.session.loggedIn) {
+        res.redirect("/profile");
+    } else {
+        let doc = fs.readFileSync("./app/index.html", "utf8");
+
+        res.set("Server", "candy");
+        res.set("X-Powered-By", "candy");
+        res.send(doc);
+    }
+});
+
+app.get("/profile", function (req, res) {
+    // check for a session
+    if (req.session.loggedIn) {
+        let profile = fs.readFileSync("./app/profile.html", "utf8");
+        let profileDOM = new JSDOM(profile);
+
+
+        profileDOM.window.document.getElementsByTagName("title")[0].innerHTML =
+            req.session.name + "'s Profile";
+        profileDOM.window.document.getElementById("profile_name").innerHTML =
+            "Welcome back! "+ req.session.name;
+
+        res.set("Server", "candy");
+        res.set("X-Powered-By", "candy");
+        res.send(profileDOM.serialize());
+    } else {
+console.log("redirecting on the server.");
+        res.redirect("/");
+    }
+});
+
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.post("/login", async function (req, res) {
+    res.setHeader("Content-Type", "application/json");
+
+    console.log("What was sent", req.body.email, req.body.password);
+    let email = req.body.email;
+	let password = req.body.password;
+
+    const mysql = require("mysql2/promise");
+    const connection = await mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "assignment6",
+    });
+    connection.connect();
+    const [rows, fields] = await connection.execute(
+        `SELECT * FROM A01209395_user WHERE email = "${req.body.email}" AND password = "${req.body.password}"`
+    );
+
+    if (rows.length > 0) {
+        req.session.loggedIn = true;
+        req.session.email = `${req.body.email}`;
+        req.session.name = `${req.body.email}`;
+        req.session.save(function (err) {
+        });
+        console.log("success, logged in");
+        res.send({ status: "success", msg: "Logged in." });
+    } else {
+        console.log("error, user not found");
+        res.send({ status: "fail", msg: "User account not found." });
+    }
+});
+
+app.get("/logout", function (req, res) {
+
+    if (req.session) {
+        req.session.destroy(function (error) {
+            if (error) {
+                res.status(400).send("Unable to log out")
+            } else {
+                // session deleted, redirect to home
+                res.redirect("/");
+            }
+        });
+    }
+});
+
+
+
+
+app.get("/table-async", function (req, res) {
+    const mysql = require("mysql2");
+    const connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "assignment6",
+    });
+    let myResults = null;
+    console.log(req.session.email)
+    connection.connect();
+    connection.query(
+        "SELECT * FROM A01209395_user WHERE email = ?", [req.session.email],
+        function (error, results, fields) {
+
+            console.log(
+                "Results from DB",
+                results,
+                "and the # of records returned",
+                results.length
+            );
+
+            myResults = results;
+            if (error) {
+
+                console.log(error);
+            }
+            let table = "<table><tr><th>ID</th><th>First Name</th><th>Last Name</th><th>Email</th></tr>";
+            for (let i = 0; i < results.length; i++) {
+                table +=
+                    "<tr><td>" +
+                    results[i].PK +
+                    "</td><td>" +
+                    results[i].first_name +
+                    "</td><td>" +
+                    results[i].last_name +
+                    "</td><td>" +
+                    results[i].email +
+                    "</td></tr>";
+            }
+            table += "</table>";
+            res.send(table);
+            connection.end();
+        }
+    );
+
+    console.log(myResults, "null");
+});
+
+
+
+app.get("/table-sync", function (req, res) {
+
+    const mysql = require("mysql2");
+    const connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "assignment6",
+    });
+    let myResults = null;
+    console.log(req.session.email)
+    connection.connect();
+    connection.query(
+        "SELECT * FROM A01209395_user_timeline, A01209395_user WHERE A01209395_user.email =? AND A01209395_user_timeline.FK = A01209395_user.PK", [req.session.email],
+        function (error, results, fields) {
+
+            console.log(
+                "Results from DB",
+                results,
+                "and the # of records returned",
+                results.length
+            );
+
+            myResults = results;
+            if (error) {
+
+                console.log(error);
+            }
+            let table = "<table><tr><th>FK-ID</th><th>Date</th><th>Text</th><th>Time</th><th>Views</th></tr>";
+            for (let i = 0; i < results.length; i++) {
+            table +=
+            "<tr><td>" +
+            results[i].FK +
+            "</td><td>" +
+            results[i].date_of_post +
+            "</td><td>" +
+            results[i].text +
+            "</td><td>" +
+            results[i].time_of_post +
+            "</td><td>" +
+            results[i].num_views +
+            "</td></tr>";
+    }
+    console.log("rows", results);
+    // don't forget the '+'
+    table += "</table>";
+    res.send(table);
+    connection.end();
+        }
+    );
+
+    console.log(myResults, "null");
+});
+
+
+// RUN SERVER
+let port = 8000;
+app.listen(port, function () {
+    console.log("Listening on port " + port + "!");
+});
