0cfdffcf80ad8ec0731e053c02f4a6b23427633c
Amarra Hong <amarrahong@gmail.com>
2022-05-04 13:11:56 -0700

    Changes to backend code, new add user page

--- server.js
+++ server.js
@@ -6,8 +6,9 @@ const {
     JSDOM
 } = require("jsdom");
 const mysql = require('mysql');
-const { response } = require("express");
-var isAdmin = false;
+const {
+    response
+} = require("express");
 
 
 
@@ -18,6 +19,8 @@ app.use("/fonts", express.static("./public/fonts"));
 app.use("/html", express.static("./public/html"));
 app.use("/media", express.static("./public/media"));
 
+
+var admin = false;
 app.use(
     session({
         secret: "secrets",
@@ -29,11 +32,9 @@ app.use(
 );
 
 app.get("/", function (req, res) {
-    console.log("1" + isAdmin);
 
     if (req.session.loggedIn) {
-        if (isAdmin === false) {
-//            res.redirect("/users");
+        if (admin === false) {
             res.redirect("/home");
         } else {
             res.redirect("/admin");
@@ -46,7 +47,7 @@ app.get("/", function (req, res) {
 });
 
 app.get("/admin", async (req, res) => {
-    if (req.session.loggedIn && isAdmin === true) {
+    if (req.session.loggedIn && admin === true) {
         let profile = fs.readFileSync("./app/admin.html", "utf-8");
         let profileDOM = new JSDOM(profile);
 
@@ -58,20 +59,10 @@ app.get("/admin", async (req, res) => {
     }
 });
 
-// app.get("/", function (req, res) {
-//     if (req.session.loggedIn) {
-//         res.redirect("/profile");
-//     } else {
-//         let doc = fs.readFileSync("./app/index.html", "utf8");
 
-//         res.set("Server", "candy");
-//         res.set("X-Powered-By", "candy");
-//         res.send(doc);
-//     }
-// });
 
 app.get("/home", async (req, res) => {
-    if (req.session.loggedIn && isAdmin === false) {
+    if (req.session.loggedIn && admin === false) {
         let profile = fs.readFileSync("./app/home.html", "utf-8");
         let profileDOM = new JSDOM(profile);
 
@@ -88,77 +79,45 @@ app.use(express.urlencoded({
     extended: true
 }));
 
-app.post("/login", function (req, res) {
+app.post("/login", async function (req, res) {
     res.setHeader("Content-Type", "application/json");
 
-    let usr = req.body.user_name;
-    let pwd = req.body.password;
-    let myResults = [];
+    console.log("What was sent", req.body.username, req.body.password);
+    let username = req.body.username;
+	let password = req.body.password;
 
-    const mysql = require("mysql2");
-    const connection = mysql.createConnection({
+    const mysql = require("mysql2/promise");
+    const connection = await mysql.createConnection({
         host: "localhost",
         user: "root",
         password: "",
-        database: "2800"
-    });
-
-    connection.connect(function (err) {
-        if (err) throw err;
-        console.log('Database is connected successfully !');
-    });
-
-    connection.execute(
-        "SELECT * FROM user WHERE user.user_name = ? AND user.password = ?", [usr, pwd],
-        function (error, results, fields) {
-            myResults = results;
-            console.log("results:", myResults);
-
-            if (req.body.user_name == myResults[0].user_name && req.body.password == myResults[0].password) {
-                if (myResults[0].admin_user === 'y') {
-                    isAdmin = true;
-                }
-                req.session.loggedIn = true;
-                req.session.user_name = myResults[0].user_name;
-                req.session.password = myResults[0].password;
-                req.session.name = myResults[0].first_name;
-                req.session.save(function (err) {});
-                res.send({
-                    status: "success",
-                    msg: "Logged in."
-                });
-            } else {
-                res.send({
-                    status: "fail",
-                    msg: "User account not found."
-                });
-            }
-            if (error) {
-                console.log(error);
-            }
-            connection.end();
-        }
-    )
-});
-app.get("/get-users", function (req, res) {
-    const connection = mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "",
-        database: "2800"
+        database: "2800",
     });
     connection.connect();
-    connection.query(
-        "SELECT user.USER_ID, user.email_address, user.first_name, user.last_name  FROM user WHERE user_removed = 'n'",
-        function (error, results) {
-            if (error) {
-                console.log(error);
-            }
-            console.log('Rows returned are: ', results);
-            res.send({ status: "success", rows: results });
-        }
+    const [rows, fields] = await connection.execute(
+        `SELECT * FROM mm_user WHERE username = "${req.body.username}" AND password = "${req.body.password}"`
     );
+
+    if (rows.length > 0) {
+        if (myResults[0].admin_user === 'y') {
+        admin = true;
+
+        req.session.loggedIn = true;
+        req.session.username = `${req.body.username}`;
+        req.session.password = `${req.body.password}`;
+        req.session.save(function (err) {
+        });
+
+    
+        console.log("success, logged in");
+        res.send({ status: "success", msg: "Logged in." });
+    } else {
+        console.log("error, user not found");
+        res.send({ status: "fail", msg: "User account not found." });
+    }
+}
 });
+
 
 app.get("/logout", function (req, res) {
 
@@ -175,50 +134,120 @@ app.get("/logout", function (req, res) {
     }
 });
 
-app.post("/user-update", function (req, res) {
-    let adminUsers = [];
-    const userId = req.params['userId'];
-    console.log(userId);
+//This post will add new account into the DB
+app.post("/add-new-user", function (req, res) {
+    res.setHeader("Content-Type", "application/json");
+
+    console.log("user name: ", req.body.userName);
+    console.log("first name: ", req.body.firstName);
+    console.log("last name: ", req.body.lastName);
+    console.log("email: ", req.body.email);
+
     const connection = mysql.createConnection({
         host: "localhost",
         user: "root",
         password: "",
-        database: "2800"
+        database: "2800",
     });
 
     connection.connect();
-    console.log(req.body.id + "ID");
-    connection.execute(
-        "SELECT * FROM user WHERE admin_user = 'y' AND user_removed = 'n'",
+
+    connection.query(
+        "INSERT INTO user (username, firstname, lastname, email, administrator, delete_user, password) values (?, ?, ?, ?, n, n)",
+        [
+            req.body.userName,
+            req.body.firstName,
+            req.body.lastName,
+            req.body.email,
+            req.body.password,
+        ],
         function (error, results, fields) {
-            adminUsers = results;
-            let send = {status: "fail", msg: "Recorded updated."};
-            connection.query("UPDATE user SET user_removed = ? WHERE USER_ID = ? AND admin_user = ?", ['y', req.body.id, 'n'], (err, rows) => {
-                if (err) {
-                    console.log(err);
-                }
-                send.status = "success";
-            });
-            if (adminUsers.length > 1) {
-                connection.query("UPDATE user SET user_removed = ? WHERE USER_ID = ? AND admin_user = ?", ['y', req.body.id, 'y'], (err, rows) => {
-                    if (err) {
-                        console.log(err);
-                    }
-                    send.status = "success";
-                });
-            } else {
-                send.status = "fail";
-            }
-            res.send(send);
             if (error) {
                 console.log(error);
             }
-            connection.end();
+            res.send({
+                status: "success",
+                msg: "Welcome to Mindful Matter!"
+            });
         }
     );
+    connection.end();
 
+    let doc = fs.readFileSync("./index.html", "utf8");
+    res.send(doc);
 });
 
+app.get("/", function (req, res) {
+    const connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "",
+        multipleStatements: true,
+    });
+
+    const createDBandTables = `CREATE DATABASE IF NOT EXISTS 2800
+        use 2800;
+        CREATE TABLE IF NOT EXISTS mm_user (
+          ID_NUMBER int NOT NULL AUTO_INCREMENT,
+          username VARCHAR(50),
+          firstname VARCHAR(50),
+          lastname VARCHAR(50),
+          email VARCHAR(50),
+          administrator VARCHAR(1),
+          delete_user VARCHAR(1),
+          password VARCHAR(50),
+          PRIMARY KEY (ID_NUMBER)
+        )`;
+
+    connection.connect();
+    connection.query(createDBandTables, function (error, results, fields) {
+        if (error) {
+            console.log(error);
+        }
+    });
+    connection.end()
+
+    async function init() {
+
+
+        const mysql = require("mysql2/promise");
+        const connection = await mysql.createConnection({
+            host: "localhost",
+            user: "root",
+            password: "",
+            multipleStatements: true
+        });
+        const createDBandTables = `CREATE DATABASE IF NOT EXISTS 2800
+        use 2800;
+        CREATE TABLE IF NOT EXISTS mm_user (
+        ID_NUMBER int NOT NULL AUTO_INCREMENT,
+        username VARCHAR(50),
+        firstname VARCHAR(50),
+        lastname VARCHAR(50),
+        email VARCHAR(50),
+        administrator VARCHAR(1),
+        delete_user VARCHAR(1),
+        password VARCHAR(50),
+        PRIMARY KEY (ID_NUMBER)
+        )`;
+        await connection.query(createDBandTables);
+
+
+        const [rows, fields] = await connection.query("SELECT * FROM mm_user");
+
+        if (rows.length == 0) {
+
+            let userRecords = "insert into user (firstname, email, password) values ?";
+            let recordValues = [
+                ["ahong", "Amarra", "Hong", "ahong@bcit.ca", "y", "n", "12345"],
+                ["gvarma", "Geetika", "Varma", "gvarma@bcit.ca", "n", "n", "12345"],
+            ];
+            await connection.query(userRecords, [recordValues]);
+        }
+        connection.end();
+        console.log("Listening on port " + port + "!");
+    }
+})
 
 //starts the server
 let port = 8000;
