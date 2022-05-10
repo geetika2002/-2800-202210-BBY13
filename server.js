6de731f529f6fb498a76b262252dfd60bf919e70
Amarra Hong <amarrahong@gmail.com>
2022-05-10 15:04:01 -0700

    Redirect to new pages, and creating admin dashboard and users page to display database information

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-"use strict"; 
+"use strict";
 const express = require("express");
 const session = require("express-session");
 const app = express();
 const fs = require("fs");
-const { JSDOM } = require("jsdom");
+const {
+    JSDOM
+} = require("jsdom");
 const mysql = require("mysql");
-const { response } = require("express");
+const {
+    response
+} = require("express");
 
 app.use("/js", express.static("./public/js"));
 app.use("/css", express.static("./public/css"));
@@ -15,147 +19,189 @@ app.use("/html", express.static("./public/html"));
 app.use("/media", express.static("./public/media"));
 
 var admin = false;
+
 app.use(
-  session({
-    secret: "secrets",
-    name: "candysession",
-    resave: false,
-    // create a unique identifier
-    saveUninitialized: true,
-  })
+    session({
+        secret: "secrets",
+        name: "candysession",
+        resave: false,
+        // create a unique identifier
+        saveUninitialized: true,
+    })
 );
 
 app.get("/", function (req, res) {
-  if (req.session.loggedIn) {
-    if (admin === false) {
-      res.redirect("/home");
+    if (req.session.loggedIn) {
+        if (admin === false) {
+            res.redirect("/home");
+        } else {
+            admin === true;
+            res.redirect("/admin");
+        }
     } else {
-      admin === true;
-      res.redirect("/admin");
+        let doc = fs.readFileSync("./app/index.html", "utf8");
+        res.send(doc);
     }
-  } else {
-    let doc = fs.readFileSync("./app/index.html", "utf8");
-    res.send(doc);
-  }
 });
 
 app.get("/home", async (req, res) => {
-  if (req.session.loggedIn && admin === false) {
-    let profile = fs.readFileSync("./app/home.html", "utf-8");
-    let profileDOM = new JSDOM(profile);
+    if (req.session.loggedIn && admin === false) {
+        let profile = fs.readFileSync("./app/home.html", "utf-8");
+        let profileDOM = new JSDOM(profile);
 
-    const mysql = require("mysql2");
+        const mysql = require("mysql2");
 
-    const connection = mysql.createConnection({
-      host: "localhost",
-      user: "root",
-      password: "",
-      database: "COMP2800",
-    });
-    connection.connect;
+        const connection = mysql.createConnection({
+            host: "localhost",
+            user: "root",
+            password: "",
+            database: "COMP2800",
+        });
+        connection.connect;
 
-    profileDOM.window.document.getElementById("first_name").innerHTML =
-      "Pleased to see you, " + req.session.name;
+        profileDOM.window.document.getElementById("first_name").innerHTML =
+            "Pleased to see you, " + req.session.name;
 
-    res.set("Server", "candy");
-    res.set("X-Powered-By", "candy");
-    res.send(profileDOM.serialize());
-  } else {
-    res.redirect("/");
-  }
+        res.set("Server", "candy");
+        res.set("X-Powered-By", "candy");
+        res.send(profileDOM.serialize());
+    } else {
+        res.redirect("/");
+    }
 });
 app.use(express.json());
 app.use(
-  express.urlencoded({
-    extended: true,
-  })
+    express.urlencoded({
+        extended: true,
+    })
 );
 
 app.get("/admin", async (req, res) => {
-  if (req.session.loggedIn && admin === true) {
-    let profile = fs.readFileSync("./app/admin.html", "utf-8");
-    let profileDOM = new JSDOM(profile);
+    if (req.session.loggedIn && admin === true) {
+        let profile = fs.readFileSync("./app/admin.html", "utf-8");
+        let profileDOM = new JSDOM(profile);
 
-    res.set("Server", "candy");
-    res.set("X-Powered-By", "candy");
-    res.send(profileDOM.serialize());
-  } else {
-    res.redirect("/");
-  }
+        res.set("Server", "candy");
+        res.set("X-Powered-By", "candy");
+        res.send(profileDOM.serialize());
+    } else {
+        res.redirect("/");
+    }
 });
 app.use(express.json());
 app.use(
-  express.urlencoded({
-    extended: true,
-  })
+    express.urlencoded({
+        extended: true,
+    })
 );
 
 app.post("/login", async function (req, res) {
-  res.setHeader("Content-Type", "application/json");
+    res.setHeader("Content-Type", "application/json");
 
 
 
-  const mysql = require("mysql2/promise");
-  const connection = await mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "COMP2800",
-  });
-  connection.connect();
-  const [rows, fields] = await connection.execute(
-    `SELECT * FROM BBY_13_mm_users WHERE username = "${req.body.username}" AND password = "${req.body.password}"`
-  );
-
-  if (rows.length > 0) {
-    req.session.loggedIn = true;
-    req.session.username = `${req.body.username}`;
-    req.session.password = `${req.body.password}`;
-    req.session.name = rows[0].firstname;
-    req.session.save(function (err) {});
-
-    res.send({
-      status: "success",
-      msg: "Logged in.",
+    const mysql = require("mysql2/promise");
+    const connection = await mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "COMP2800",
     });
-    if (rows[0].administrator == "y") {
-      admin = true;
+    connection.connect();
+    const [rows, fields] = await connection.execute(
+        `SELECT * FROM BBY_13_mm_users WHERE username = "${req.body.username}" AND password = "${req.body.password}"`
+    );
+
+    if (rows.length > 0) {
+        req.session.loggedIn = true;
+        req.session.username = `${req.body.username}`;
+        req.session.password = `${req.body.password}`;
+        req.session.name = rows[0].firstname;
+        req.session.save(function (err) {});
+
+        res.send({
+            status: "success",
+            msg: "Logged in.",
+        });
+        if (rows[0].administrator == "y") {
+            admin = true;
+        } else {
+            admin = false;
+        }
     } else {
-      admin = false;
+        res.send({
+            status: "fail",
+            msg: "User account not found.",
+        });
     }
-  } else {
-    console.log("error, user not found");
-    res.send({
-      status: "fail",
-      msg: "User account not found.",
-    });
-  }
 });
 
 app.get("/logout", function (req, res) {
-  if (req.session) {
-    req.session.destroy(function (error) {
-      if (error) {
-        res.status(400).send("Unable to log out");
-      } else {
-        admin = false;
-        let doc = fs.readFileSync("./app/index.html", "utf8");
-        res.send(doc);
-      }
-    });
-  }
+    if (req.session) {
+        req.session.destroy(function (error) {
+            if (error) {
+                res.status(400).send("Unable to log out");
+            } else {
+                admin = false;
+                let doc = fs.readFileSync("./app/index.html", "utf8");
+                res.send(doc);
+            }
+        });
+    }
 });
 
-async function init() {
-  const mysql = require("mysql2/promise");
-  const connection = await mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    multipleStatements: true,
-  });
+//ALL PAGE REDIRECTS GO HERE
+app.get("/admin-dash", function(req, res) {
+    if (req.session) {
+        let doc = fs.readFileSync("./app/admin-dash.html", "utf8");
+        res.send(doc);
+    }
+});
 
-  const createDBAndTables = `CREATE DATABASE IF NOT EXISTS COMP2800;
+app.get("/user-profiles", function(req, res) {
+    if (req.session) {
+        let doc = fs.readFileSync("./app/user-profiles.html", "utf8");
+        res.send(doc);
+    }
+});
+
+app.get("/home", function(req, res) {
+    if (req.session) {
+        let doc = fs.readFileSync("./app/home.html", "utf8");
+        res.send(doc);
+    }
+});
+
+app.get("/profile", function(req, res) {
+    if (req.session) {
+        let doc = fs.readFileSync("./app/profile.html", "utf8");
+        res.send(doc);
+    }
+});
+
+app.get("/admin", function(req, res) {
+    if (req.session) {
+        let doc = fs.readFileSync("./app/admin.html", "utf8");
+        res.send(doc);
+    }
+});
+
+
+
+
+//ALL PAGE REDIRECTS END HERE
+
+
+async function init() {
+    const mysql = require("mysql2/promise");
+    const connection = await mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "",
+        multipleStatements: true,
+    });
+
+    const createDBAndTables = `CREATE DATABASE IF NOT EXISTS COMP2800;
                             use COMP2800;
                             CREATE TABLE IF NOT EXISTS BBY_13_mm_users (
                                 ID_NUMBER int NOT NULL AUTO_INCREMENT,
@@ -168,24 +214,24 @@ async function init() {
                                 password VARCHAR(50),
                                 PRIMARY KEY (ID_NUMBER));`;
 
-  await connection.query(createDBAndTables);
+    await connection.query(createDBAndTables);
 
-  const [userRows, userFields] = await connection.query(
-    "SELECT * FROM BBY_13_mm_users"
-  );
+    const [userRows, userFields] = await connection.query(
+        "SELECT * FROM BBY_13_mm_users"
+    );
 
-  if (userRows.length == 0) {
-    let userRecord =
-      "insert into BBY_13_mm_users (username, firstname, lastname, email, administrator, delete_user, password) values ?";
-    let userValue = [
-      ["ahong", "Amarra", "Hong", "ahong@bcit.ca", "y", "n", "12345"],
-      ["gvarma", "Geetika", "Varma", "gvarma@bcit.ca", "n", "n", "12345"],
-      ["sbae", "Sam", "Bae", "sbae@bcit.ca", "y", "n", "12345"],
-      ["joh", "Jason", "Oh", "joh@bcit.ca", "y", "n", "12345"],
-    ];
-    await connection.query(userRecord, [userValue]);
-  }
-  console.log("Listening on port " + port + "!");
+    if (userRows.length == 0) {
+        let userRecord =
+            "insert into BBY_13_mm_users (username, firstname, lastname, email, administrator, delete_user, password) values ?";
+        let userValue = [
+            ["ahong", "Amarra", "Hong", "ahong@bcit.ca", "y", "n", "12345"],
+            ["gvarma", "Geetika", "Varma", "gvarma@bcit.ca", "n", "n", "12345"],
+            ["sbae", "Sam", "Bae", "sbae@bcit.ca", "y", "n", "12345"],
+            ["joh", "Jason", "Oh", "joh@bcit.ca", "y", "n", "12345"],
+        ];
+        await connection.query(userRecord, [userValue]);
+    }
+    console.log("Listening on port " + port + "!");
 }
 
 let port = 8000;
